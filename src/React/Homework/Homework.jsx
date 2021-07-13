d7ad1494f9c7d9a79ce35927c0bfeea35c9a5e80
Nurbol Batkhan <nurbolbatkhan@gmail.com>
2021-07-12 22:56:23 -0500

    final update

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -12,14 +12,15 @@ const Homework = () => {
             >
                 <p>
                     Git is a version control system that tracks changes in any set of files. It allows team to work together in one project without worring each individual is doing.
-                    Its goals include speed, data integrity, and support distributed, non-linear work flows.
+                    Its goals include speed, data integrity, and support distributed, non-linear work flows. Git is a software, maintained by linux, focused on version control and code sharing. Again, its  a version control system to manage source code history.
                 </p>
                 <p>
-                    Heroku is a cloud platform where you can locate your code and deploy. You can place your application and show to the others. You can use many languages such as Ryby, Java, Node.js, Scala,Python, and PHP.
+                    Heroku is a cloud platform where you can locate your code and deploy. You can place your application and show to the others. You can use many languages such as Ryby, Java, Node.js, Scala,Python, and PHP. You can host your code and create a website like sample.herokuapp.com. 
+                    Its a good place to show your work to others.
                 </p>
                 <p>
-                    Github is a place to push our code and play well with others. It also allows to show your projects to others. You can create a repositories. 
-                    Someone can work on their local computers and merge their works. Good thing is its open source and most of the codes are open.
+                    Github is a place to push our code and play well with others. Its a cloud-based hosting service that lets you manage git repositories. It also allows to show your projects to others. You can create a repositories. 
+                    Someone can work on their local computers and merge their works. Good thing is its open source and most of the codes are open. Github is a service, a graphical user interface, hosted on the web and focused on centralized source code hosting.
                 </p>
             </Essay>
             <Essay
