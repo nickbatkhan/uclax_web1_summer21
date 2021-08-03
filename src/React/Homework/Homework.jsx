09b62ab9b59a6cb1e25b8e0a610e6d97398cde04
Nurbol Batkhan <nurbolbatkhan@gmail.com>
2021-08-02 19:54:26 -0500

    Updating Homework

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -88,7 +88,46 @@ const Homework = () => {
                     Finally, web designer, front end developer and back end developer, they all work on one thing and final product is one. Its important in practice that everyone is working together.
                 </p>
             </Essay>
+            <Essay
+                number = { 5 }
+                question='Distinguish the difference between Site Relative, Document Relative, and Absolute Paths.'
+            >
+                <p>
+                    It is very helpful to distinguish the difference between site relative, document relative and absolute relative when it comes to linking to page or file on the site. 
+                </p>
+                <p>
+                    An absolute URL contains the entire address from the protocol (HTTPS) to the domain name (www.example.com) and includes the location within your website in your folder system (/foldernameA or /foldernameB) names within the URL. Basically, it's the full URL of the page that you link to.
+                </p>
+                <p>
+                    The reason to choose Absolute path is that first, Quicker Coding, large websites are made much easier to code when you shorten your URL into a relative format.
+                    Second, Staging Environment, If you are using a content management system that has staging environment with its own unique domain like WordPress or SharePoint, then your entire website is replicated on that staging domain. The relative format allows the same website to exist on staging and production domain, or the live accessible version of your website, without having to go back in and re-code all of the URLs. This not only makes coding easier for a web developer but also serves as a time saver.
+                    Finally, Faster Load Times, pages that use relative URLs will load more quickly than pages that use absolute URLs, for the most part, although the difference is minuscule at best. 
+                </p>
+                    The relative URL, on the other hand, does not use the full web address and only contains the location following the domain. It assumes that the link you add is on the same site and is part of the same root domain. The relative path starts with the forward slash and leads the browser to stay within the current site.
+                <p>
+                    The reasons to choose relative path, Foils Scrapers, for URLs using the absolute method, it's harder for people to scrape information from your site directory using scraper programs. If you have all of your internal links as relative URLs, it would be very easy for a scraper to simply scrape your entire website and put it up on a new domain.
+                    Additionally, Disallows Duplicate Content. It’s very important to use absolute URLs in order to avoid duplicate content issues. Imagine you have multiple versions of root domains that are indexed in Google without a canonical tag that points to the correct version of the site.
+                </p>
+            </Essay>
 
+            <Essay
+                number = { 6 }
+                question='What is the difference between jpg, gif, png and SVG images?'
+            >
+                <p>
+                    Joint Photographic Experts Group or A JPG is optimized for photographs. JPG works by getting rid of the things that the eye is not likely to see or notice. JPGs are lossy raster files which means they lose a small amount of information every-time the JPG is saved. JPGs work with compressing color so they are great for photographs and complex digital imagery. A high-resolution jpg is usually also suitable these days for printing with most print-on-demand companies.
+                </p>
+                <p>
+                    GIF or Graphics Interchange Format. This bitmap format supports up to 8 bits per pixel which limits its color palette to 256. Not good for photographs an are mostly known these days for simply animated memes. GIFs are not huge players in the web design world. 
+                </p>
+                <p>
+                    Portable Network Graphic or PNG was designed for use on the Internet and is the most largely used image compression format on the web. PNGs are not for printing professional graphic images. PNGs are raster graphics and were made as an improvement to the GIF. PNGs work well with line art images and photographs for websites but can result in really large file sizes. PNGs also offer alpha transparency which is very cool because each individual pixel has its own level of opacity. PNGs come in two types PNG-8 and PNG-24. PNG-8 means that the file has only 8 bits per pixel and the PNG-24 has 24 bits per pixel.
+                </p>
+                    Scaleable Vector Graphic or SVGs are vector graphics that support animation and their behaviors are defined in XML files for they can be searched, indexed, scripted and compressed. Vector graphics are great for logos and line art because they aren't made from pixels and can be infinitely resized without losing quality or increasing in size. SVGs are your best option when creating icons and line art for high-resolution displays.
+                <p>
+                    Best use of JPG is Digital Images & photography, for GIF: sample animations, for PNG transparent images, iconic graphics and for SVG logos, line art, icons & animations. GIF has 256 colors which is relatively smaller than others. JPG has no transparency, GIF is binary, PNG is yes, SVG is yes. JPG is best for photos and PNG is good. 
+                </p>
+            </Essay>
         </div>
     );
 } 
